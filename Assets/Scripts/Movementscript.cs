4c9236abbd6efc4c2377af3b1eb58ed5f519f758
Goodest person <mflens22@student.aau.dk>
2025-03-27 12:50:59 +0100

    jump, fall and land animations
    added to player

--- Assets/Scripts/Movementscript.cs
+++ Assets/Scripts/Movementscript.cs
@@ -11,24 +11,35 @@ public class Playermovement : MonoBehaviour
     public float horizontalInput;
     public float speed = 8f;
     public float jumpPower;
+    public bool grounded;
+
+    public Animator Animation;
 
     //below all are used for basic player stuff
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    
+
+    private void Start()
+    {
+        Animation = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(horizontalInput*speed, rb.linearVelocity.y);
         
+        
+        
         // jumps the player according to its x direction and jump power (is in update to not miss button press)
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            Debug.Log("jumps");
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+            Jump();
         }
+        Animation.SetBool("IsMoving", horizontalInput !=0);
+        Animation.SetBool("IsGround", grounded);
 
         if (horizontalInput > 0.01f)
         {
@@ -40,11 +51,22 @@ public class Playermovement : MonoBehaviour
         }
     }
 
-    // returns a true or false depending on if the overlap circle is well overlapping with the ground layer integer on the layer list (set this in the editor)
-    private bool isGrounded()
+    
+    void Jump()
     {
-        
-        Debug.Log("is grounded");
-        return Physics2D.OverlapCircle(groundCheck.position, 0.5f, groundLayer);
+        Animation.SetTrigger("Jump");
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+        Debug.Log(rb.linearVelocity.y);
+        grounded = false;
+
+    }
+
+    // returns a true or false depending on if the overlap circle is well overlapping with the ground layer integer on the layer list (set this in the editor)
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
     }
 }
