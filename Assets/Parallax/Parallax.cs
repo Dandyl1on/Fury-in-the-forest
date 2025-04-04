cae3a9da47a07751c9eed40c1cf8882b1b1ea532
Bragh2001 <112693932+Bragh2001@users.noreply.github.com>
2025-04-04 13:16:24 +0200

    Parallax Fucking workS!!

--- Assets/Parallax/Parallax.cs
+++ Assets/Parallax/Parallax.cs
@@ -3,14 +3,18 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
 
-    private float length, startpos;
+    private float length;
     public GameObject cam;
     public float parallaxEffect;
 
+    public Vector2 startpos;
+
     void Start()
     {
-        startpos = transform.position.x;
+        startpos = transform.position;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
+
+
     }
 
     void FixedUpdate()
@@ -18,15 +22,15 @@ public class Parallax : MonoBehaviour
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
 
-        transform.position = new Vector3 (startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos.x + dist, startpos.y, transform.position.z);
 
-        if(temp > startpos + length) 
-        { 
-            startpos += length;
-        }
-        else if(temp < startpos - length)
+        if (temp > startpos.x + length)
         {
-            startpos -= length;
+            startpos.x += length;
+        }
+        else if (temp < startpos.x - length)
+        {
+            startpos.x -= length;
         }
     }
 }
